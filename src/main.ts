25c35b3ab4ca809b626e9a7881a12fbfc7088373
MohamedElsayed002 <mohamed.madoo2016@gmail.com>
2026-01-21 16:37:55 +0200

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet'
 
 // npm install --production
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
